bf9980f4b5f1a9bf0673d9b0689616dacedf8ceb
Oduntan Wasiu <oduntanabidemi02@gmail.com>
2024-12-19 11:32:24 -0800

    first commit

---
+++ modify.js
@@ -0,0 +1,16 @@
+var logoLink = document.querySelector('.logoLink');
+logoLink.classList.remove('interact-button');
+logoLink.setAttribute('href', '#');
+
+let pageHeader = document.querySelector('.header');
+pageHeader.style.background = 'red';
+document.addEventListener('scroll', function() {
+  let scrollPosition = window.scrollY;
+
+  // Change background color depending on scroll position
+  if (scrollPosition > 100) {
+      pageHeader.style.backgroundColor = 'red';
+  } else {
+      pageHeader.style.backgroundColor = 'transparent';
+  }
+});
